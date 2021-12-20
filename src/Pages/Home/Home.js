53083e6068955543eb0d81574789a0ff9f941713
pragashraj <rajpragashraj30@gmail.com>
2021-12-20 09:57:59 +0530

    feat: project initiated with material ui

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+import './Home.css'
+
+class Home extends Component {
+    render() {
+        return (
+            <div className = 'home_root_container'>
+                
+            </div>
+        )
+    }
+}
+
+export default Home
