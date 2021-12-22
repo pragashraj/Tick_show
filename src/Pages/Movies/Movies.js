05e3cfc76031061658fd6203edb73a70b97b7562
pragashraj <rajpragashraj30@gmail.com>
2021-12-22 16:45:04 +0530

    feat: movies page initiated

---
+++ src/Pages/Movies/Movies.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+import './Movies.css'
+
+class Movies extends Component {
+    render() {
+        return (
+            <div className = 'movies_root_container'>
+                
+            </div>
+        )
+    }
+}
+
+export default Movies
---
+++ src/Pages/Theatres/Theatres.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+import './Theatres.css'
+
+class Theatres extends Component {
+    render() {
+        return (
+            <div className = 'theatres_root_container'>
+                
+            </div>
+        )
+    }
+}
+
+export default Theatres
