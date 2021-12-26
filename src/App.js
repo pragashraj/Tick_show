05a0375f41f903b8e09988463dedb56ab84e9fda
pragashraj <rajpragashraj30@gmail.com>
2021-12-26 12:59:10 +0530

    feat: movie page filter ui

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import Theatres from './Pages/Theatres/Theatres'
+import Movies from './Pages/Movies/Movies'
 import NavBar from './Components/NavBar'
 import Footer from './Components/Footer/Footer'
 
@@ -8,7 +8,7 @@ const App = () => {
     return (
         <div>
             <NavBar/>
-            <Theatres/>
+            <Movies/>
             <Footer/>
         </div>
     )
--- src/Pages/Movies/Movies.js
+++ src/Pages/Movies/Movies.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
 
 import ContentList from '../../Components/ContentList/ContentList'
+import Filter from '../../Components/Filter.js/Filter'
+
+//Material-UI
+import { Grid } from '@mui/material'
 
 import './Movies.css'
 import movieCardImage1 from '../../assets/images/1.jpg'
@@ -11,13 +15,22 @@ import movieCardImage5 from '../../assets/images/5.jpg'
 
 class Movies extends Component {
     state = {
+        filters: [],
         movies_block: [],
         nowShowingExpanded: true,
-        upcomingExpanded: false
+        upcomingExpanded: false,
+        categoryChecked: [0]
     }
 
     componentDidMount() {
         //for creating dummy data list
+        this.createDataBlock()
+
+        //for create filter list
+        this.createFilters()
+    }
+
+    createDataBlock = () => {
         let dummyDataArray = []
         let dummyBlockArray = ["Now Showing", "Upcoming Movies"]
 
@@ -36,6 +49,23 @@ class Movies extends Component {
         })
 
         this.setState({ movies_block: dummyDataArray })
+    }
+
+    createFilters = () => {
+        const filterArray = ["Language", "Experience", "Genre"]
+        const filterElementContents = {
+            "Language": ["English", "Tamil", "Sinhala"],
+            "Experience": ["2D", "3D"],
+            "Genre": ["Action", "Adventure", "Comedy"]
+        }
+        
+        let array = []
+        filterArray.forEach(element => {
+            const filterElement = {label: element, list: filterElementContents[element]}
+            array.push(filterElement)
+        })
+
+        this.setState({ filters: array })
     }
 
     getExpandStateAttribute = (title) => {
@@ -61,8 +91,63 @@ class Movies extends Component {
 
     }
 
-    render() {
+    renderFilters = () => {
+        const {categoryChecked, filters} = this.state
+        return (
+            <Grid container spacing = {2} sx = {{marginTop: "10px"}}>
+                { filters.map(item => {
+                    const {label, list } = item
+                    return (
+                        <Grid item xs = {4} sm = {4} md = {12}>
+                            <Filter 
+                                label = {label}
+                                list = {list}
+                                checked = {categoryChecked}
+                            />
+                        </Grid>
+                    )
+                }) }
+            </Grid>
+        )
+    }
+
+    renderMoviesBlockExtended = () => {
+        return (
+            <Grid container spacing = {2}>
+                <Grid item xs = {12} sm = {4} md = {2}>
+                    <span className = 'filter_header_title'>Filter by</span>
+                    { this.renderFilters() }
+                </Grid>
+                <Grid item xs = {12} sm = {8} md = {10}>
+                    
+                </Grid>
+            </Grid>
+        )
+    }
+
+    renderMoviesBlock = () => {
         const {movies_block} = this.state
+        return (
+            <div className = 'movies_block'>
+                { movies_block.map((item, idx) => {
+                    const {title, list} = item
+                    const name = this.getExpandStateAttribute(title)
+                    return (
+                        <ContentList 
+                            title = {title}
+                            listItems = {list}
+                            expanded = {this.state[name]}
+                            handleExpandOnClick = {this.handleExpandOnClick}
+                            handleBuyOnClick = {this.handleBuyTicketOnClick}
+                            key = {idx}
+                        />
+                    ) 
+                }) }
+            </div>
+        )
+    }
+
+    render() {
         return (
             <div className = 'movies_root_container'>
                 <div className = 'movies_header'>
@@ -70,20 +155,7 @@ class Movies extends Component {
                 </div>
                 <div className = 'parallax'>
                     <div className = 'movies_block'>
-                        { movies_block.map((item, idx) => {
-                            const {title, list} = item
-                            const name = this.getExpandStateAttribute(title)
-                            return (
-                                <ContentList 
-                                    title = {title}
-                                    listItems = {list}
-                                    expanded = {this.state[name]}
-                                    handleExpandOnClick = {this.handleExpandOnClick}
-                                    handleBuyOnClick = {this.handleBuyTicketOnClick}
-                                    key = {idx}
-                                />
-                            ) 
-                        }) }
+                        { this.renderMoviesBlockExtended() }
                     </div>
                 </div>
             </div>
