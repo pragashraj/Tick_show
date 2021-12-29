2d856870c1395b9b8d222ff7b8ccbe6ea891082a
pragashraj <rajpragashraj30@gmail.com>
2021-12-29 12:58:20 +0530

    feat: new theatre card

--- src/Pages/Theatres/Theatres.js
+++ src/Pages/Theatres/Theatres.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 
-import TheatreCard from '../../Components/TheatreCard'
+import Sorter from '../../Components/Sorter'
+import TheatreListItem from '../../Components/TheatreListItem'
 
 //Material-UI
 import Grid from '@mui/material/Grid'
@@ -10,33 +11,99 @@ import theatreImage from '../../assets/images/Theatres.jpg'
 
 class Theatres extends Component {
     state = {
-        theatreList: ["1", "2", "3", "4", "5", "6"]
+        location: "Colombo",
+        experience: "2D",
+        theatreList: []
     }
 
-    renderTheatrCard = () => {
+    sort_data = [
+        {name: "location", label: "Location", menuItems: ["Colombo", "Jaffna"]},
+        {name: "experience", label: "Experience", menuItems: ["2D", "3D"]}
+    ]
+
+    componentDidMount() {
+        //for creating dummy data list
+        this.createDataBlock()
+    }
+
+    createDataBlock = () => {
+        const theatre = {
+            name: "Ja-ela Cinemax", 
+            src: theatreImage, 
+            address: "Negombo-Colombo Main Rd, Ja-Ela 11350", 
+            contact: "0117 549 650", 
+            imdb: "99%",
+            timeSlots: ["5:00 am", "8:00 am", "11:00 am", "4:00 pm", "7:00 pm", "10:00 pm"] 
+        }
+
+        const dummyArr = ["1", "2", "3", "4"]
+        let data = []
+
+        dummyArr.forEach(e => {
+            data.push(theatre)
+        })
+
+        this.setState({ theatreList: data })
+    }
+
+    handleSortOnChange = (e) => {
+        const {name, value} = e.target
+        this.setState({ [name]: value })
+    }
+
+    handleTimeSlotOnClick = () => {
+
+    }
+
+    handleLikeOnClick = () => {
+        
+    }
+
+    renderTheatreListItem = (item, idx) => {
         return (
-            <TheatreCard 
-                title = {"Ja-ela Cinemax"}
-                otherInfo = {{add: "Negombo-Colombo Main Rd, Ja-Ela 11350", no: "0117 549 650"}}
-                imageSrc = {theatreImage}
-                timeVariants = {["5:00 am", "8:00 am", "11:00 am", "4:00 pm", "7:00 pm", "10:00 pm"]}
-            />
+            <Grid item xs = {12} sm = {6} md = {6} key = {idx}>
+                <TheatreListItem 
+                    item = {item}
+                    handleLikeOnClick = {this.handleLikeOnClick}
+                    handleTimeSlotOnClick = {this.handleTimeSlotOnClick}
+                />
+            </Grid>
         )
     }
 
     renderTheatresList = () => {
+        const {theatreList} = this.state
         return (
             <div className = 'theatres_list'>
                 <Grid container spacing = {2}>
-                    { this.state.theatreList.map(i => {
-                        return (
-                            <Grid item xs = {12} sm = {6} md = {6}>
-                                { this.renderTheatrCard() }                
-                            </Grid>
-                        )
-                    }) }
+                    { theatreList.map((i, idx) => this.renderTheatreListItem(i, idx)) }
                 </Grid>
             </div>
+        )
+    }
+
+    renderSort = () => {
+        const {location, experience} = this.state
+        const values = {location, experience}
+        return (
+            <Sorter 
+                sort_data = {this.sort_data} 
+                handleChange = {this.handleSortOnChange}
+                values = {values}
+            />
+        )
+    }
+
+    renderTheatresBlockExtended = () => {
+        return (
+            <Grid container spacing = {2}>
+                <Grid item xs = {12} sm = {12} md = {12}>
+                    { this.renderSort() }
+                </Grid>
+                <Grid item xs = {12} sm = {12} md = {12}>
+                    { this.renderTheatresList() }
+                </Grid>
+            </Grid>
         )
     }
 
@@ -49,7 +116,7 @@ class Theatres extends Component {
                 <div className = 'parallax'>
                     <div className = 'theatres_block'>
                         <h1>Our Theatres</h1>
-                        { this.renderTheatresList() }
+                        { this.renderTheatresBlockExtended() }
                     </div>
                 </div>
             </div>
