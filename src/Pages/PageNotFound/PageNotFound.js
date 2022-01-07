5c9772f8ebd02a00cc2e4887d221c0642a65ecaf
pragashraj <rajpragashraj30@gmail.com>
2022-01-07 11:36:23 +0530

    feat: Page not found page

--- src/Pages/PageNotFound/PageNotFound.js
+++ src/Pages/PageNotFound/PageNotFound.js
@@ -5,8 +5,14 @@ import './PageNotFound.css'
 class PageNotFound extends Component {
     render() {
         return (
-            <div>
-                
+            <div className = 'page_not_found_root'>
+                <div className = "page_not_found_container">
+                    <div className = "notfound-404">
+                        <h1>404</h1>
+                        <h2>Page not found</h2>
+                    </div>
+                    <a href = "/">Homepage</a>
+                </div>
             </div>
         )
     }
