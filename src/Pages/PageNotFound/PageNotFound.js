6492aa46f169432b1782cc5876e4b66b2e3709bd
pragashraj <rajpragashraj30@gmail.com>
2022-01-07 11:26:53 +0530

    feat: Page not found page initiated

---
+++ src/Pages/PageNotFound/PageNotFound.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+import './PageNotFound.css'
+
+class PageNotFound extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default PageNotFound
