f2c9495af5fee099694dcc6ce5db0c052c6d2493
Pragashraj <rajpragashraj30@gmail.com>
2022-01-29 08:41:11 +0530

    feat: root & auth reducers

---
+++ src/redux/reducers/rootReducer.js
@@ -0,0 +1,17 @@
+import {combineReducers} from 'redux'
+import authReducer from './authReducer'
+
+import {persistReducer} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const persistConfig = {
+    key: 'root',
+    storage
+}
+
+const rootReducer = combineReducers({
+    auth: authReducer
+})
+
+
+export default persistReducer(persistConfig, rootReducer)
