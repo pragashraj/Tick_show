a23a6aebc010e6f054642da121315b8df7ff8690
Pragashraj <rajpragashraj30@gmail.com>
2022-01-31 09:35:50 +0530

    feat: search movie

--- src/api/home.js
+++ src/api/home.js
@@ -1,4 +1,4 @@
-import {GET} from './core'
+import {GET, POST} from './core'
 
 export const getEndpointWithPrefix = (endpoint) => {
     return `landingPage/${endpoint}`
@@ -7,4 +7,9 @@ export const getEndpointWithPrefix = (endpoint) => {
 export const getContents = () => {
     const endpoint =  getEndpointWithPrefix(`get-landingPage-contents`)
     return GET(endpoint)
+}
+
+export const searchMovie = (data) => {
+    const endpoint =  getEndpointWithPrefix(`search-movie`)
+    return POST(endpoint, data)
 }
