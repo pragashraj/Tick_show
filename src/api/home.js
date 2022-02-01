7889de25491475ebf0aa2d063af9313d635a97f6
Pragashraj <rajpragashraj30@gmail.com>
2022-02-01 16:45:20 +0530

    feat: footer -> subscribe newsletter

--- src/api/home.js
+++ src/api/home.js
@@ -13,3 +13,8 @@ export const searchMovie = (data) => {
     const endpoint =  getEndpointWithPrefix(`search-movie`)
     return POST(endpoint, data)
 }
+
+export const subscribeToNewsletter = (email) => {
+    const endpoint =  getEndpointWithPrefix(`subscribe-newsletter/${email}`)
+    return POST(endpoint)
+}
