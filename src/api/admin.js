b4c113a7dc15d90395ed64a1af1729a4f232968b
Pragashraj <rajpragashraj30@gmail.com>
2022-02-10 16:24:03 +0530

    feat: create new event api

--- src/api/admin.js
+++ src/api/admin.js
@@ -4,12 +4,17 @@ export const getEndpointWithPrefix = (endpoint) => {
     return `admin/${endpoint}`
 }
 
-export const createNewMovies = (formData, token) => {
+export const createNewMovie = (formData, token) => {
     const endpoint =  getEndpointWithPrefix('create-new-movie')
     return MULTIPART(endpoint, formData, token)
 }
 
 export const createNewTheatre = (formData, token) => {
     const endpoint =  getEndpointWithPrefix('create-new-theatre')
+    return MULTIPART(endpoint, formData, token)
+}
+
+export const createNewEvent = (formData, token) => {
+    const endpoint =  getEndpointWithPrefix('create-new-event')
     return MULTIPART(endpoint, formData, token)
 }
