23c10e6f483da6a63430133cf015216772b66dcf
Pragashraj <rajpragashraj30@gmail.com>
2022-02-13 11:25:41 +0530

    feat: admin panel -> sidebar initiated

--- src/Pages/Admin/AdminPanel.js
+++ src/Pages/Admin/AdminPanel.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react'
 
+
+//Material-UI
+import {Grid} from '@mui/material'
+
 import './AdminPanel.css'
 
 class AdminPanel extends Component {
+
+    renderSideBar = () => {
+        return (
+            <div className = 'panel-side-bar'></div>
+        )
+    }
+
+    renderMainContainer = () => {
+        return (
+            <div className = 'panel-main-container'>
+                <Grid container spacing = {2}>
+                    <Grid item xs = {12} sm = {2} md = {3}>
+                        { this.renderSideBar() }
+                    </Grid>
+                    <Grid item xs = {12} sm = {10} md = {9}></Grid>
+                </Grid>
+            </div>
+        )
+    }
+
     render() {
         return (
-            <div className = 'admin-panel-root'></div>
+            <div className = 'admin-panel-root'>
+                { this.renderMainContainer() }
+            </div>
         )
     }
 }
