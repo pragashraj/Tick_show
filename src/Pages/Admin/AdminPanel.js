3153b4b4d502b3434176e55266b8475ea4d49231
Pragashraj <rajpragashraj30@gmail.com>
2022-02-13 11:14:22 +0530

    feat: admin panel page initiated

---
+++ src/Pages/Admin/AdminPanel.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+import './AdminPanel.css'
+
+class AdminPanel extends Component {
+    render() {
+        return (
+            <div className = 'admin-panel-root'></div>
+        )
+    }
+}
+
+export default AdminPanel
