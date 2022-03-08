e33f7885b7aa8a7663d6a28d05ed3e71547a45ef
Pragashraj <rajpragashraj30@gmail.com>
2022-03-08 10:07:23 +0530

    feat: update or delete initialized

---
+++ src/Pages/Admin/UpdateOrDelete.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class UpdateOrDelete extends Component {
+    render() {
+        return (
+            <div>UpdateOrDelete</div>
+        )
+    }
+}
+
+export default UpdateOrDelete
