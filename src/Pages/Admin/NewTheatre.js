a03055d5a146385611f658aee77e2e27efaecb6d
Pragashraj <rajpragashraj30@gmail.com>
2022-03-08 09:24:34 +0530

    feat: new theatre initialized

---
+++ src/Pages/Admin/NewTheatre.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class NewTheatre extends Component {
+    render() {
+        return (
+            <div>NewTheatre</div>
+        )
+    }
+}
+
+export default NewTheatre
